e9da34b371ac51af278ad12b4b2f64a1ff028c94
Name <email@example.ru>
2022-05-26 00:25:39 +0300

    Add project files.

---
+++ Module8_Unit6_Task1/Module8_Unit6_Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Module8_Unit6_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Ввели путь к папке
+            string Path;
+            Console.WriteLine("Введите путь к папке:");
+            Path = Console.ReadLine();
+            ClearDirectory(Path);
+        }
+
+        static void ClearDirectory(string FolderName)
+        {
+             DirectoryInfo dirInfo = new DirectoryInfo(@$"{FolderName}");
+            try
+            {
+                
+                if (dirInfo.Exists)
+                {
+                    
+                    foreach (FileInfo fi in dirInfo.GetFiles())
+                    {
+                        if ((DateTime.Now - fi.LastAccessTime) > TimeSpan.FromMinutes(30))
+                        { fi.Delete(); }
+                    }
+                    foreach (DirectoryInfo di in dirInfo.GetDirectories())
+                    {
+                        ClearDirectory(di.FullName);
+                        if ((DateTime.Now - di.LastAccessTime) > TimeSpan.FromMinutes(30))
+                        { di.Delete(); }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }      
+
+
+
+        
+
+
+        
+    }
+}
